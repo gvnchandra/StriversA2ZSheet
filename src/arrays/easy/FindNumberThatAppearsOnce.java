d500bc8ea7d8f32477dc40314a7ab04e0c6bdcbd
gvnchandra <gvnchandra1215@gmail.com>
2023-10-26 22:50:12 +0530

    arrays single element change

--- src/arrays/easy/FindNumberThatAppearsOnce.java
+++ src/arrays/easy/FindNumberThatAppearsOnce.java
@@ -8,11 +8,28 @@ public class FindNumberThatAppearsOnce {
     }
 
     public static int getSingleElement(int [] nums){
-        //However the solution is failing for one testcase in codestudio
-        int xor=0;
-        for (int num : nums) {
-            xor ^= num;
+        //However the solution is failing for one testcase in codestudio because of timelimit
+//        int xor=0;
+//        for (int num : nums) {
+//            xor ^= num;
+//        }
+//        return xor;
+
+        int low = 0;
+        int high = nums.length - 1;
+        int mid;
+        while (low < high) {
+            mid = (low + high) / 2;
+            if (mid % 2 != 0) {
+                mid -= 1;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
         }
-        return xor;
+
+        return nums[low];
     }
 }
